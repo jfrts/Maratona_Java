be222849b7cda0f808dc55c6d2f9324428853fcf
Júlio Freitas <jcsr.frts@gmail.com>
2023-03-11 19:08:49 -0300

    001 - Tipos primitivos

---
+++ Introducao/A001_TiposPrimitivos.java
@@ -0,0 +1,19 @@
+package Introducao;
+
+public class A001_TiposPrimitivos {
+    
+    public static void main(String[] args) {
+        // Tipos primitivos: int, double, short, long, boolean, char, byte, float
+        int idade = 67;
+        double salarioDouble = 3000D;
+        float salarioFloat = 3000F;
+        long salarioLong = 3000L;
+        byte idadeByte = 67;
+        short salarioShort = 3000;
+        boolean verdadeiro = true;
+        char caractere = 'M';
+
+        System.out.println("A idade é " + idade);
+    }
+
+}
