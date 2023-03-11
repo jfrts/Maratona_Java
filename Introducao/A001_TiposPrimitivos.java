fb4bb4463835c430e32d6ca83358cc245ea46f02
Júlio Freitas <jcsr.frts@gmail.com>
2023-03-11 19:11:52 -0300

    002 - String

--- Introducao/A001_TiposPrimitivos.java
+++ Introducao/A001_TiposPrimitivos.java
@@ -6,7 +6,7 @@ public class A001_TiposPrimitivos {
         // Tipos primitivos: int, double, short, long, boolean, char, byte, float
         int idade = 67;
         double salarioDouble = 3000D;
-        float salarioFloat = 3000F;
+        float salarioFloat = 3000.50F;
         long salarioLong = 3000L;
         byte idadeByte = 67;
         short salarioShort = 3000;
---
+++ Introducao/A002_String.java
@@ -0,0 +1,15 @@
+package Introducao;
+
+public class A002_String {
+    
+    public static void main(String[] args) {
+        // String é um tipo complexo de dado, não é primitivo.
+        // Usado para guardar cadeia de caracteres.
+
+        String nomeCriadorDoJava = "James Gosling";
+
+        System.out.println("O nome do criador do JAVA é: " + nomeCriadorDoJava);
+
+    }
+
+}
