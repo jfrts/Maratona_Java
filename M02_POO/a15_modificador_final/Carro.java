6ef2fca87d9fa7125223a28806b69111efb9d621
Júlio Freitas <jcsr.frts@gmail.com>
2023-03-24 14:52:32 -0300

    Aula 78 - Modificador final pt. 1 tipo primitivo

---
+++ M02_POO/a15_modificador_final/Carro.java
@@ -0,0 +1,19 @@
+package M02_POO.a15_modificador_final;
+
+public class Carro {
+    private String nome;
+    public static final double VELOCIDADE_LIMITE = 250;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public static double getVelocidadeLimite() {
+        return VELOCIDADE_LIMITE;
+    }
+
+}
---
+++ M02_POO/a15_modificador_final/Main.java
@@ -0,0 +1,7 @@
+package M02_POO.a15_modificador_final;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
