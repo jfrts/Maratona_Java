59f10734b27b8622adc9911354b6af7516cecf97
Júlio Freitas <jcsr.frts@gmail.com>
2023-03-25 15:15:34 -0300

    Aula 85 - Métodos Abstratos

--- M02_POO/a17_classes_abstratas/Desenvolvedor.java
+++ M02_POO/a17_classes_abstratas/Desenvolvedor.java
@@ -1,7 +1,14 @@
 package M02_POO.a17_classes_abstratas;
 
 public class Desenvolvedor extends Funcionario {
+    private final int PORCENTUAL_DE_BONUS = 10;
+
     public Desenvolvedor(String nome, double salario) {
         super(nome, salario);
+    }
+
+    @Override
+    public void calculaBonus() {
+        this.salario = this.salario + (this.salario * this.PORCENTUAL_DE_BONUS / 100);
     }
 }
--- M02_POO/a17_classes_abstratas/Gerente.java
+++ M02_POO/a17_classes_abstratas/Gerente.java
@@ -1,7 +1,14 @@
 package M02_POO.a17_classes_abstratas;
 
 public class Gerente extends Funcionario {
+    private final int PORCENTUAL_DE_BONUS = 8;
+
     public Gerente(String nome, double salario) {
         super(nome, salario);
+    }
+
+    @Override
+    public void calculaBonus() {
+        this.salario = this.salario + (this.salario * this.PORCENTUAL_DE_BONUS / 100);
     }
 }
