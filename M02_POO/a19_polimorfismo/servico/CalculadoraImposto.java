4726d76c58882a1baf4071a49704b1a5b16c9d61
Júlio Freitas <jcsr.frts@gmail.com>
2023-03-27 14:48:19 -0300

    Aula 93 - Polimorfismo pt.4 Cast e Instanceof

--- M02_POO/a19_polimorfismo/servico/CalculadoraImposto.java
+++ M02_POO/a19_polimorfismo/servico/CalculadoraImposto.java
@@ -1,6 +1,7 @@
 package M02_POO.a19_polimorfismo.servico;
 
 import M02_POO.a19_polimorfismo.dominio.Produto;
+import M02_POO.a19_polimorfismo.dominio.Tomate;
 
 public class CalculadoraImposto {
     public static void calcularImposto(Produto produto) {
@@ -9,5 +10,10 @@ public class CalculadoraImposto {
         System.out.println("Produto: " + produto.getNome());
         System.out.println("Valor: " + produto.getValor());
         System.out.println("Imposto: " + imposto);
+
+        if (produto instanceof Tomate) {
+            Tomate tomate = (Tomate) produto;
+            System.out.println(tomate.getDataValidade());
+        }
     }
 }
