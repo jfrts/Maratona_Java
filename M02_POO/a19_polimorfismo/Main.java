e68b51e90bc8f9510b9a9debfb92f964aca03882
Júlio Freitas <jcsr.frts@gmail.com>
2023-03-27 14:16:09 -0300

    Aula 91 - Polimorfismo pt. 2 Funcionamento

---
+++ M02_POO/a19_polimorfismo/Main.java
@@ -0,0 +1,10 @@
+package M02_POO.a19_polimorfismo;
+
+import M02_POO.a19_polimorfismo.dominio.Produto;
+import M02_POO.a19_polimorfismo.dominio.Tomate;
+
+public class Main {
+    public static void main(String[] args) {
+        Produto p1 = new Tomate("Tomate Cereja", 10, "27/04/2023");
+    }
+}
--- M02_POO/a19_polimorfismo/dominio/Tomate.java
+++ M02_POO/a19_polimorfismo/dominio/Tomate.java
@@ -2,14 +2,24 @@ package M02_POO.a19_polimorfismo.dominio;
 
 public class Tomate extends Produto {
     public static final double PORCENTAGEM_DE_IMPOSTO = 3;
+    private String dataValidade;
 
-    public Tomate(String nome, double valor) {
+    public Tomate(String nome, double valor, String dataValidade) {
         super(nome, valor);
+        this.dataValidade = dataValidade;
     }
 
     @Override
     public double calcularImposto() {
         System.out.println("Calculando imposto do computador...");
         return this.valor * PORCENTAGEM_DE_IMPOSTO / 100;
+    }
+
+    public String getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(String dataValidade) {
+        this.dataValidade = dataValidade;
     }
 }
