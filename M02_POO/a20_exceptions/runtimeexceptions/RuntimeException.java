8e1fa8135ab098dd5b08930738f572452f16bcc5
Júlio Freitas <jcsr.frts@gmail.com>
2023-03-28 15:57:23 -0300

    Aula 96 - pt. 2 Runtime

--- M02_POO/a20_exceptions/runtimeexceptions/RuntimeException.java
+++ M02_POO/a20_exceptions/runtimeexceptions/RuntimeException.java
@@ -3,9 +3,15 @@ package M02_POO.a20_exceptions.runtimeexceptions;
 public class RuntimeException {
     public static void main(String[] args) {
         
+        // Unchecked:
+
         // NullPointerException
         Object obj = null;
         // System.out.println(obj.toString());
+
+        // ArrayIndexOutOfBoundsException
+        int[] nums = {1, 2};
+        // System.out.println(nums[2]);
 
     }
 }
