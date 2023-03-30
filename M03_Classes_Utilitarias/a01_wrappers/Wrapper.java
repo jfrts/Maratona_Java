75e2be8cdfdddcf223c32953332cf73ae31270e7
Júlio Freitas <jcsr.frts@gmail.com>
2023-03-30 16:37:08 -0300

    Aula 106 - Classes utilitárias - Wrappers pt1

---
+++ M03_Classes_Utilitarias/a01_wrappers/Wrapper.java
@@ -0,0 +1,16 @@
+package M03_Classes_Utilitarias.a01_wrappers;
+
+public class Wrapper {
+    // Wrappers são objetos que encapsulam os tipos primitivos.
+    // São usados para possibilitar a passagem da referência dos tipos primitivos nos parâmetros. 
+    public static void main(String[] args) {
+        Byte biteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Float floatW = 10F;
+        Long longW = 10L;
+        Double doubleW = 10D;
+        Character charW = 'A';
+        Boolean boolW = false;
+    }
+}
