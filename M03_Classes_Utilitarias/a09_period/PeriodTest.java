19c7b68d331a0c709d4f3c56f66d754393866d07
Júlio Freitas <jcsr.frts@gmail.com>
2023-04-04 15:49:36 -0300

    Aula 124 - ChronoUnit

--- M03_Classes_Utilitarias/a09_period/PeriodTest.java
+++ M03_Classes_Utilitarias/a09_period/PeriodTest.java
@@ -1,7 +1,9 @@
 package M03_Classes_Utilitarias.a09_period;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class PeriodTest {
     public static void main(String[] args) {
@@ -10,8 +12,18 @@ public class PeriodTest {
 
         System.out.println(Period.between(date1, date2));
 
-        Period p1 = Period.ofWeeks(58); 
+        Period p1 = Period.ofWeeks(508);
         System.out.println(p1); // P406D
-        System.out.println(p1.getMonths()); // 0. Não funciona pegarmos o número de mêses dessa forma. Precisamos da classe ChronoUnit.
+        System.out.println(p1.getMonths()); // 0. Não funciona pegarmos o número de mêses dessa forma. Precisamos da
+                                            // classe ChronoUnit.
+
+        // ChronoUnit
+        System.out.println(
+                date1.until(date1.plusDays(p1.getDays()), ChronoUnit.YEARS)); // se p1 = 58 retorna 1, se 508 retorna 9
+
+        LocalDateTime aniversario = LocalDateTime.of(1994, 11, 21, 8, 15, 45);
+        System.out.println(aniversario); // 1994-11-21T08:15:45
+
+        System.out.println(ChronoUnit.DAYS.between(aniversario, LocalDateTime.now())); // 10361 dias
     }
 }
