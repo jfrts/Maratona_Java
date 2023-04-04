354266ecb33ea4084ef967c83a5fa45756f8ac23
Júlio Freitas <jcsr.frts@gmail.com>
2023-04-03 22:21:31 -0300

    Aula 116 - Internacionalização Números

--- M03_Classes_Utilitarias/a03_date/LocaleTest.java
+++ M03_Classes_Utilitarias/a03_date/LocaleTest.java
@@ -1,20 +1,42 @@
 package M03_Classes_Utilitarias.a03_date;
 
 import java.text.DateFormat;
+import java.text.NumberFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
 public class LocaleTest {
     public static void main(String[] args) {
-        Locale locale = new Locale("IT", "IT");
-        Locale locale2 = new Locale("IT", "CH");
+        // Locale locale = new Locale("IT", "IT");
+        // Locale locale2 = new Locale("IT", "CH");
 
-        Calendar calendar = Calendar.getInstance();
+        // Calendar calendar = Calendar.getInstance();
 
-        DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL, locale);
-        DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, locale2);
+        // DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL, locale);
+        // DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, locale2);
 
-        System.out.println("Itália: " + df1.format(calendar.getTime()));
-        System.out.println("Suíça: " + df2.format(calendar.getTime()));
+        // System.out.println("Itália: " + df1.format(calendar.getTime()));
+        // System.out.println("Suíça: " + df2.format(calendar.getTime()));
+
+        // Aula 116
+        // System.out.println(Locale.getDefault()); // en_US
+
+        // Number format
+        Locale localeBR = new Locale("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIT = Locale.ITALY;
+
+        NumberFormat[] nf = new NumberFormat[4];
+        nf[0] = NumberFormat.getInstance();
+        nf[1] = NumberFormat.getInstance(localeBR);
+        nf[2] = NumberFormat.getInstance(localeJP);
+        nf[3] = NumberFormat.getInstance(localeIT);
+
+        double valor = 10_000.2130;
+
+        for (NumberFormat numberFormat : nf) {
+            System.out.println(numberFormat.format(valor));
+        }
+
     }
 }
