e6894ec542245abd15dd5365593a5f3047444fbe
Júlio Freitas <jcsr.frts@gmail.com>
2023-04-04 10:12:43 -0300

    Aula 117 - Internacionalização de Moeda

--- M03_Classes_Utilitarias/a03_date/LocaleTest.java
+++ M03_Classes_Utilitarias/a03_date/LocaleTest.java
@@ -2,6 +2,7 @@ package M03_Classes_Utilitarias.a03_date;
 
 import java.text.DateFormat;
 import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -32,11 +33,40 @@ public class LocaleTest {
         nf[2] = NumberFormat.getInstance(localeJP);
         nf[3] = NumberFormat.getInstance(localeIT);
 
-        double valor = 10_000.2130;
+        double valor = 1_000.2130;
 
+        System.out.println("\nNumber Format");        
         for (NumberFormat numberFormat : nf) {
+            numberFormat.setMaximumFractionDigits(2);
             System.out.println(numberFormat.format(valor));
         }
 
+        // Currency format
+        
+        NumberFormat[] cFormat = new NumberFormat[4];
+        cFormat[0] = NumberFormat.getCurrencyInstance();
+        cFormat[1] = NumberFormat.getCurrencyInstance(localeBR);
+        cFormat[2] = NumberFormat.getCurrencyInstance(localeJP);
+        cFormat[3] = NumberFormat.getCurrencyInstance(localeIT);
+        
+        System.out.println("\nCurrency Format");        
+        for (NumberFormat currencyFormat : cFormat) {
+            System.out.println(currencyFormat.format(valor));
+        }
+
+        
+        System.out.println("\nParse");
+        
+        String valorString = "1000.2130";
+        /*
+         * String valorString = "1_000.2130"; => Return 1; Isso ocorre por conta da String conter um valor inválido '_'.
+         * String valorString = "1000.2130"; => Return 1000.213;
+         */
+        try {
+            System.out.println(nf[0].parse(valorString));
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
