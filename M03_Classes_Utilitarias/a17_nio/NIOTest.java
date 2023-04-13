6e85956794b206a45e4ef19e8b6401c3dbee1f3a
Júlio Freitas <jcsr.frts@gmail.com>
2023-04-13 11:41:43 -0300

    145 - NIO pt 02 - Path, Paths, Files pt 02

--- M03_Classes_Utilitarias/a17_nio/NIOTest.java
+++ M03_Classes_Utilitarias/a17_nio/NIOTest.java
@@ -1,16 +1,44 @@
 package M03_Classes_Utilitarias.a17_nio;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 /**
  * A equipe java criou a interface PATH para substituir a classe File
  */
 public class NIOTest {
     public static void main(String[] args) {
-        Path path = Paths.get("M03_Classes_Utilitarias", "a17_nio");
-        System.out.println(path);
+        Path path = Paths.get("M03_Classes_Utilitarias", "a17_nio", "pasta");
 
-        System.out.println(path.toAbsolutePath());
+        if (Files.notExists(path)) {
+            try {
+                Files.createDirectories(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Path filePath = Paths.get(path.toString(), "file.txt");
+
+        if (Files.notExists(filePath)) {
+            try {
+                Files.createFile(filePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Path source = filePath;
+        Path target = Paths.get(path.toString(), "file-renamed.txt");
+        
+        try {
+            Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+            Files.delete(source);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
